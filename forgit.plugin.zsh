df8e4eab0d6235775f1f9d6b20fc0178d8404654
Wenxuan <wenxuangm@gmail.com>
2018-06-02 23:52:01 +0800

    Support user custom fzf options for forgit (#4)
    Former-commit-id: 2dc83917ec1b02cbbd45c94d8a49e4b45ca3cb58

--- forgit.plugin.zsh
+++ forgit.plugin.zsh
@@ -1,19 +1,20 @@
 forgit::fzf() {
-    fzf --border \
-        --ansi \
-        --cycle \
-        --reverse \
-        --height '80%' \
-        --bind='alt-v:page-up' \
-        --bind='ctrl-v:page-down' \
-        --bind='alt-k:preview-up,alt-p:preview-up' \
-        --bind='alt-j:preview-down,alt-n:preview-down' \
-        --bind='alt-a:select-all' \
-        --bind='ctrl-r:toggle-all' \
-        --bind='ctrl-s:toggle-sort' \
-        --bind='?:toggle-preview' \
-        --preview-window="right:60%" \
-        --bind='alt-w:toggle-preview-wrap' "$@"
+    FZF_DEFAULT_OPTS="
+        $FZF_DEFAULT_OPTS
+        --ansi
+        --height '80%'
+        --bind='alt-v:page-up'
+        --bind='ctrl-v:page-down'
+        --bind='alt-k:preview-up,alt-p:preview-up'
+        --bind='alt-j:preview-down,alt-n:preview-down'
+        --bind='alt-a:select-all'
+        --bind='ctrl-r:toggle-all'
+        --bind='ctrl-s:toggle-sort'
+        --bind='?:toggle-preview'
+        --preview-window='right:60%'
+        --bind='alt-w:toggle-preview-wrap'
+        $FORGIT_FZF_DEFAULT_OPTS
+    " fzf "$@"
 }
 
 forgit::color_to_grep_code() {
@@ -59,7 +60,7 @@ forgit::log() {
     forgit::inside_work_tree || return 1
     local cmd="echo {} |grep -o '[a-f0-9]\{7\}' |head -1 |xargs -I% git show --color=always % $forgit_emojify $forgit_fancy"
     eval "git log --graph --color=always --format='%C(auto)%h%d %s %C(black)%C(bold)%cr' $@ $forgit_emojify" |
-        forgit::fzf -e +s --tiebreak=index \
+        forgit::fzf +s +m --tiebreak=index \
             --bind="enter:execute($cmd |LESS='-R' less)" \
             --preview="$cmd"
 }
@@ -69,7 +70,7 @@ forgit::diff() {
     forgit::inside_work_tree || return 1
     local cmd="git diff --color=always -- {} $forgit_emojify $forgit_fancy"
     git ls-files --modified $(git rev-parse --show-toplevel)|
-        forgit::fzf -e -0 \
+        forgit::fzf +m -0 \
             --bind="enter:execute($cmd |LESS='-R' less)" \
             --preview="$cmd"
 }
@@ -86,7 +87,7 @@ forgit::add() {
     local files=$(git -c color.status=always status --short |
         grep -e "$added" -e "$changed" -e "$unmerged" |
         awk '{printf "[%10s]  ", $1; $1=""; print $0}' |
-        forgit::fzf -e -0 -m --nth 2..,.. \
+        forgit::fzf -0 -m --nth 2..,.. \
             --preview="git diff --color=always -- {-1} $forgit_emojify $forgit_fancy" |
         cut -d] -f2 |
         sed 's/.* -> //') # for rename case
@@ -100,13 +101,13 @@ forgit:ignore() {
     [ -f $FORGIT_GI_INDEX ] || forgit::ignore::update
     local preview="echo {} |awk '{print \$2}' |xargs -I% bash -c 'cat $FORGIT_GI_CACHE/% 2>/dev/null || (curl -sL https://www.gitignore.io/api/% |tee $FORGIT_GI_CACHE/%)'"
     IFS=$'\n'
-    [[ $# -gt 0 ]] && args=($@) || args=($(cat $FORGIT_GI_INDEX |nl -nrn -w4 -s'  ' |fzf -m --preview="$preview" --preview-window="right:70%" |awk '{print $2}'))
+    [[ $# -gt 0 ]] && args=($@) || args=($(cat $FORGIT_GI_INDEX |nl -nrn -w4 -s'  ' |forgit::fzf -m --preview="$preview" --preview-window="right:70%" |awk '{print $2}'))
     test -z "$args" && return 1
     local options=(
     '(1) Output to stdout'
     '(2) Append to .gitignore'
     '(3) Overwrite .gitignore')
-    opt=$(printf '%s\n' "${options[@]}" |fzf |awk '{print $1}')
+    opt=$(printf '%s\n' "${options[@]}" |forgit::fzf +m |awk '{print $1}')
     case "$opt" in
         '(1)' )
             forgit::ignore::get ${args[@]}
