e6e3ba4ce53ec786c44f32546833e9e143165f12
carlfriedrich <carlfriedrich@posteo.de>
2022-11-24 15:00:59 +0100

    Meta: GitHub tag action: add git log to output for debugging purposes

--- .github/scripts/tag.sh
+++ .github/scripts/tag.sh
@@ -4,6 +4,8 @@ set -e
 
 head_tag=$(git describe --exact-match 2>/dev/null || true)
 
+git log --color=always --format="%C(auto)%h %s%d" | head
+
 if [[ ${head_tag} =~ [\d{2}\.\d{2}\.\d+] ]]
 then
     echo "Current master already has version tag ${head_tag}"
@@ -13,6 +15,7 @@ else
     version=$(date +'%y.%m.0')
     git tag -a ${version} -m "Release ${version}"
     git push origin ${version}
+    git log --color=always --format="%C(auto)%h %s%d" | head -1
     echo "Pushed new tag:"
     echo "${REPO_URL}/releases/tag/${version}"
 fi
