837056dc5d4c3db667c662765bae49f18d0f5138
LittleHaku <ivandelhorno@gmail.com>
2025-11-20 18:55:22 +0100

    fish completions

--- completions/git-forgit.fish
+++ completions/git-forgit.fish
@@ -8,7 +8,7 @@
 function __fish_forgit_needs_subcommand
     for subcmd in add blame branch_delete checkout_branch checkout_commit checkout_file checkout_tag \
         cherry_pick cherry_pick_from_branch clean diff fixup ignore log reflog rebase reset_head \
-        revert_commit reword squash stash_show stash_push
+        revert_commit reword squash stash_show stash_push switch_branch
         if contains -- $subcmd (commandline -opc)
             return 1
         end
@@ -45,6 +45,7 @@ complete -c git-forgit -n __fish_forgit_needs_subcommand -a show -d 'git show vi
 complete -c git-forgit -n __fish_forgit_needs_subcommand -a squash -d 'git squash'
 complete -c git-forgit -n __fish_forgit_needs_subcommand -a stash_show -d 'git stash viewer'
 complete -c git-forgit -n __fish_forgit_needs_subcommand -a stash_push -d 'git stash push selector'
+complete -c git-forgit -n __fish_forgit_needs_subcommand -a switch_branch -d 'git switch branch selector'
 
 complete -c git-forgit -n '__fish_seen_subcommand_from add' -a "(complete -C 'git add ')"
 complete -c git-forgit -n '__fish_seen_subcommand_from branch_delete' -a "(__fish_git_local_branches)"
@@ -66,3 +67,4 @@ complete -c git-forgit -n '__fish_seen_subcommand_from show' -a "(complete -C 'g
 complete -c git-forgit -n '__fish_seen_subcommand_from squash' -a "(complete -C 'git log ')"
 complete -c git-forgit -n '__fish_seen_subcommand_from stash_show' -a "(__fish_git_complete_stashes)"
 complete -c git-forgit -n '__fish_seen_subcommand_from stash_push' -a "(__fish_git_files modified deleted modified-staged-deleted)"
+complete -c git-forgit -n '__fish_seen_subcommand_from switch_branch' -a "(complete -C 'git switch ')"
