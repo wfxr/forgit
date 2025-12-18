0e1de0bcdfe37103779d08cdd52646664de4fb7e
sandroid <sandroid@posteo.net>
2025-12-18 15:12:00 +0100

    test: ignore global git config when running tests

--- tests/empty-state.test.sh
+++ tests/empty-state.test.sh
@@ -3,6 +3,10 @@
 function set_up_before_script() {
     source bin/git-forgit
 
+    # Ignore global git config files
+    export GIT_CONFIG_SYSTEM=/dev/null
+    export GIT_CONFIG_GLOBAL=/dev/null
+
     # Create a temporary git repository for testing
     cd "$(bashunit::temp_dir)" || return 1
     git init -q
